49e8722cbf793534e79daf88918501c8db5f48e7
Sergio <sergio.defrutos@campusfp.es>
2021-03-22 14:12:10 +0100

    Otro commit mas

---
+++ SegundoProyecto/ejercicio3.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simplificacion Fraccional</title>
+</head>
+<body>
+    <form action="ejercicio3.php" method="post">
+		Numerador: <input type="decimal" name="numerador" size="10">
+		Denominador: <input type="decimal" name="denominador" size="10">
+		<input type="submit" value="simplificar">
+	</form>
+	
+    <?php
+    if (isset($_POST['simplificar'])) {
+        $numerador = $_POST["numerador"];
+        $denominador = $_POST["denominador"];
+        $divisor = 2;
+        
+        while ($divisor<=$numerador && $divisor <= $denominador){
+            while(($numerador % $divisor) == 0 && ($denominador % $divisor) == 0){
+                $numerador = $numerador/$divisor;
+                $denominador = $denominador/$divisor;
+            }
+            $divisor++;
+        }
+        
+        echo "Fraccion simplificada: ".$numerador."\\".$denominador;
+    ?>
+    
+</body>
+</html>
